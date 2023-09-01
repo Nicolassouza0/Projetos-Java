57bf77e15b8b08ba7ad419c986d38ffdfc1d584c
Nicolassouza0 <123048143+Nicolassouza0@users.noreply.github.com>
2023-08-31 21:06:26 -0300

    Add files via upload

---
+++ Subtracao.java
@@ -0,0 +1,19 @@
+package exemplos;
+
+import java.util.Scanner;
+
+public class Subtracao {
+	public static void main(String[]args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro valor a ser subtraido, sendo que deve ser inteiro: ");
+		int valor1=sc.nextInt();
+		
+		System.out.println("Digite o segundo valor a ser subtraido, sendo que deve ser inteiro: ");
+		int valor2=sc.nextInt();
+
+		int subtrair=valor1-valor2;
+		System.out.println("O resultado é: "+subtrair);
+	}
+}
