55ca71d25e3b646704cd35b7ba5dc31485ed9086
Nicolassouza0 <123048143+Nicolassouza0@users.noreply.github.com>
2023-08-31 21:08:27 -0300

    Add files via upload

---
+++ CarroObjeto.java
@@ -0,0 +1,15 @@
+package introducao;
+
+public class CarroObjeto {
+
+	public static void main(String[] args) {
+		Carro BMW = new Carro();
+		BMW.cor="branco";
+		BMW.motor=2.0;
+		BMW.porta=4;
+		BMW.ligado();
+		BMW.estado();
+
+	}
+
+}
