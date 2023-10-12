55f246401ddbd2531b19c0bf51b992eac07c5825
Nicolas Souza <123048143+Nicolassouza0@users.noreply.github.com>
2023-10-11 21:56:14 -0300

    Add files via upload

---
+++ Controle.java
@@ -0,0 +1,10 @@
+package introducao;
+
+public interface Controle {
+	
+	public abstract  void  aumentaVolume ( );
+    public abstract  void  diminuiVolume (  );
+    public abstract void  mudaCanal (double x );
+    public abstract void ligar();
+    public abstract void desligar();
+}
